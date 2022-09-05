605d41b526618d63bd476cbc3496d917c82824a7
mustafa-sarshar <musti.sar4@gmail.com>
2022-09-05 17:38:22 +0200

    update

--- utils/create-fake-datasets/app.py
+++ utils/create-fake-datasets/app.py
@@ -2,7 +2,8 @@
 This python script will create some tables including random data for educational purposes.
 """
 #%% Import Libs
-import sys
+import sys, os
+from pathlib import Path
 import pandas as pd
 import names
 from random_word import RandomWords
@@ -11,7 +12,8 @@ from csv import writer
 
 #%% Init a database
 rw = RandomWords()
-OPERATIONS_LENGTH_LIMIT = 1000
+CUR_DIR = os.getcwd()
+# OPERATIONS_LENGTH_LIMIT = 1000
 COUNTRIES = [               # Source: https://pytutorial.com/python-get-random-country (accessed on 05.09.2022)
     ('US', 'United States'),
     ('AF', 'Afghanistan'),
@@ -277,6 +279,29 @@ def get_a_random_team_name():
         team_name = rw.get_random_word(hasDictionaryDef="true", includePartOfSpeech="noun,verb", minCorpusCount=1, maxCorpusCount=10, minDictionaryCount=1, maxDictionaryCount=10, minLength=5, maxLength=10)
     return team_name.capitalize()
 
+def append_list_as_row(file_name, list_of_elem):        # Source: https://thispointer.com/python-how-to-append-a-new-row-to-an-existing-csv-file/#:~:text=Open%20our%20csv%20file%20in,in%20the%20associated%20csv%20file
+    # Open file in append mode
+    with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer = writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(list_of_elem)
+
+def init_datasets():
+    if os.path.exists(Path(CUR_DIR, "teams.csv")):
+        os.remove(Path(CUR_DIR, "teams.csv"))
+    if os.path.exists(Path(CUR_DIR, "contracts.csv")):
+        os.remove(Path(CUR_DIR, "contracts.csv"))
+    if os.path.exists(Path(CUR_DIR, "players.csv")):
+        os.remove(Path(CUR_DIR, "players.csv"))
+    
+    cols = ["team_id", "name", "country"]
+    append_list_as_row(file_name=Path(CUR_DIR, "teams.csv"), list_of_elem=cols)
+    cols = ["contract_id", "player_id", "salary"]
+    append_list_as_row(file_name=Path(CUR_DIR, "contracts.csv"), list_of_elem=cols)
+    cols = ["player_id", "gender", "first_name", "last_name", "age", "team", "birthplace"]
+    append_list_as_row(file_name=Path(CUR_DIR, "players.csv"), list_of_elem=cols)
+
 def create_dataset_team(num=0):
     lst_teams = []
     for idx_team in range(num):
@@ -290,6 +315,18 @@ def create_dataset_team(num=0):
         lst_teams.append(dict_team)
         if TRACE: print(lst_teams[-1])
     return lst_teams
+
+def append_dataset_team_csv(file_name="teams.csv", num=0):
+    for idx_team in range(num):
+        team_name = get_a_random_team_name()
+        # team_name = "Team " + str(idx_team+1)
+        row_contents = [
+            idx_team,
+            team_name,
+            choice(COUNTRIES)[1]
+        ]
+        append_list_as_row(file_name, row_contents)
+        if TRACE: print(row_contents)
 
 def create_dataset_contracts(num=0):
     lst_contracts = []
@@ -308,6 +345,21 @@ def create_dataset_contracts(num=0):
             idx_contract += 1
     return lst_contracts
 
+def append_dataset_contracts_csv(file_name="contracts.csv", num=0):
+    idx_contract = 0
+    for idx_player in range(num):
+        n_contracts = randint(1, 5)
+        for _ in range(n_contracts):
+            salary = int(random() * SALARY_SCALE_FACTOR[int(random() * len(SALARY_SCALE_FACTOR))]) + SALARY_OFFSET
+            row_contents = [
+                idx_contract,
+                idx_player,
+                salary
+            ]
+            append_list_as_row(file_name, row_contents)
+            if TRACE: print(row_contents)
+            idx_contract += 1
+
 def create_dataset_players(num=0):
     lst_players = []
     for idx_player in range(num):
@@ -325,44 +377,54 @@ def create_dataset_players(num=0):
         if TRACE: print(lst_players[-1])
     return lst_players
 
-def append_list_as_row(file_name, list_of_elem):
-    # Open file in append mode
-    with open(file_name, 'a+', newline='') as write_obj:
-        # Create a writer object from csv module
-        csv_writer = writer(write_obj)
-        # Add contents of list as last row in the csv file
-        csv_writer.writerow(list_of_elem)
+def append_dataset_players_csv(file_name="players.csv", num=0):
+    for idx_player in range(num):
+        gender = choice(GENDERS)
+        row_contents = [
+            idx_player,
+            gender,
+            names.get_first_name(gender=gender),
+            names.get_last_name(),
+            randint(AGE_RANGE[0], AGE_RANGE[1]),
+            randint(0, N_TEAMS),
+            choice(COUNTRIES)[1]
+        ]
+        append_list_as_row(file_name, row_contents)
+        if TRACE: print(row_contents)
+
 #### Generate fake data
-#%% Create the fake team dataset
-num = 0
-if N_TEAMS > OPERATIONS_LENGTH_LIMIT:
-    num = OPERATIONS_LENGTH_LIMIT - N_TEAMS
-    lst_teams = create_dataset_team(N_TEAMS)
-    df_teams = pd.DataFrame(lst_teams)
+#%% Create the fake teams dataset
+init_datasets()
 
-
+"""
+lst_teams = create_dataset_team(N_TEAMS)
+df_teams = pd.DataFrame(lst_teams)
 df_teams.to_csv("teams.csv", index=False)
 if DEBUG: print(f"{len(df_teams)} teams generated and saved as .csv file.")
 del df_teams, lst_teams
+"""
+append_dataset_team_csv(num=N_TEAMS)
 
 #%%
-
-
+"""
 lst_contracts = create_dataset_contracts(N_PLAYERS)
 df_contracts = pd.DataFrame(lst_contracts)
 df_contracts.to_csv("contracts.csv", index=False)
 if DEBUG: print(f"{len(df_contracts)} contracts generated and saved as .csv file.")
 del df_contracts, lst_contracts
+"""
+append_dataset_contracts_csv(num=N_PLAYERS)
 
 #%%
-
-
-
+"""
 lst_players = create_dataset_players(N_PLAYERS)
 df_players = pd.DataFrame(lst_players)
 df_players.to_csv("players.csv", index=False)
 if DEBUG: print(f"{len(df_players)} players generated and saved as .csv file.")
 del df_players, lst_players
+"""
+
+append_dataset_players_csv(num=N_PLAYERS)
 
 # %%
 print("Finish!")
